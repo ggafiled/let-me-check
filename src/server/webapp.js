20bf30d8455a0ad39ae14332ab8146e85c407899
ggafiled <gafewkik234@gmail.com>
2021-06-01 15:13:01 +0700

    edit project properties.

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -1,44 +1,41 @@
-const {
-    render
-} = require('../functions/utils');
+const { render } = require('../functions/utils');
 
-var { replyMessage, MESSAGE_TYPE } = require('../functions/LineBot');
+const { replyMessage, MESSAGE_TYPE } = require('../functions/LineBot');
 
 const Route = {};
-Route.path = function(routeName, callback) {
-    Route[routeName] = callback;
+Route.path = function (routeName, callback) {
+  Route[routeName] = callback;
 };
 
 Route.path('project-list', () => {
-    return render('index', {
-        title: '- 🕵️‍♀️ Project List -',
-    });
+  return render('index', {
+    title: '- 🕵️‍♀️ Project List -',
+  });
 });
 
 const doGet = (e) => {
-
-    if (Route[e.parameters.v]) {
-        return Route[e.parameters.v]();
-    }
-    return render('404');
+  if (Route[e.parameters.v]) {
+    return Route[e.parameters.v]();
+  }
+  return render('404');
 };
 
 const doPost = async (e) => {
-    const data = JSON.parse(e.postData.contents);
-    Logger.log(`[doPost()] data: ${JSON.stringify(data)}`);
-    const messages = data.events[0].message.text;
-    Logger.log(`[doPost()] messages: ${messages}`);
-  
-    replyMessage(data.events[0].replyToken, data.events[0].message.text, MESSAGE_TYPE.NORMAL);
+  const data = JSON.parse(e.postData.contents);
+  Logger.log(`[doPost()] data: ${JSON.stringify(data)}`);
+  const messages = data.events[0].message.text;
+  Logger.log(`[doPost()] messages: ${messages}`);
 
-    return ContentService.createTextOutput(
-      JSON.stringify({
-        status: 'ok',
-      })
-    ).setMimeType(ContentService.JSON);
-  };
+  replyMessage(data.events[0].replyToken, data.events[0].message.text, MESSAGE_TYPE.NORMAL);
+
+  return ContentService.createTextOutput(
+    JSON.stringify({
+      status: 'ok',
+    })
+  ).setMimeType(ContentService.JSON);
+};
 
 module.exports = {
-    doPost,
-    doGet,
+  doPost,
+  doGet,
 };
